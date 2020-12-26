9ac28c3aeb3dd5684c8673b002bcf16143ddaa89
FrancoisBasset <>
2020-12-26 17:58:13 +0100

    port 3000

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(80, function() {
-	console.log('Start on 80');
+app.listen(3000, function() {
+	console.log('Start on 3000');
 });
 
 app.use(express.static('./public'));
